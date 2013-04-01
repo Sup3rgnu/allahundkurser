2826d9e9ae4d7397eb5599d158236c905dade2aa
Mikael <sup3rgnu@gmail.com>
2013-04-01 11:50:28 +0200

    cleanup

--- db/migrate/20130316132457_create_course_tags.rb
+++
@@ -1,9 +0,0 @@
-class CreateCourseTags < ActiveRecord::Migration
-  def change
-    create_table :course_tags do |t|
-      t.integer :course_id
-      t.integer :tag_id
-      t.timestamps
-    end
-  end
-end
--- test/unit/course_tags_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class CourseTagsTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
