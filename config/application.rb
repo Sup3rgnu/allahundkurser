138c332b5fd919bbc8d8dc79a105c371167475ff
Mikael <sup3rgnu@gmail.com>
2013-04-01 17:08:19 +0200

    moved css calls to application

--- config/application.rb
+++ config/application.rb
@@ -58,7 +58,7 @@ module Allahundkurser
 
     config.assets.initialize_on_precompile = false
     
-    config.assets.precompile += ['jquery-ui-timepicker-addon.css']
+    config.assets.precompile += ['jquery-ui-timepicker-addon.css.scss']
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
