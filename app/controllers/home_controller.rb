6c26b4d4f3b40be9f204a5ef832290410c4d3e5e
Sup3rgnu <sup3rgnu@gmail.com>
2013-06-02 13:10:31 +0200

    Fixed tag filtering on startpage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,20 +1,19 @@
 class HomeController < ApplicationController
   def index
 	@courses = Course.find(:all, :order => "id desc", :limit => 10)
-	@courses = @courses.select { |c| c.province == params[:province_filter] } if !params[:province_filter].blank?
-
+	@locations = Location.all
 	@courseTags = CourseTag.all 
 
-	@courseTags.each do |ct|
-		@c = Array.new
-		if ct.tag_id == params[:tag_filter]
-			@c.push ct.course_id
-		end
-	end
+	# Filter on provinces 
+	@courses = @courses.select { |c| c.province == params[:province_filter] } if !params[:province_filter].blank?
 
-	@courses = @courses.select { |c| @c.include? c.id } if !params[:tag_filter].blank?
-	
-	@locations = Location.all
+	# Filter on tags
+	@matches = Array.new
+	@courseTags.each do |ct|
+		@matches.push ct.course_id if ct.tag_id == params[:tag_filter].to_i
+	end
+	@courses = @courses.select { |c| @matches.include? c.id } if !params[:tag_filter].blank?
+
     respond_to do |format|
     	format.html # show.html.erb
 		format.json { render :json => { :courses => @courses, :locations => @locations}}
